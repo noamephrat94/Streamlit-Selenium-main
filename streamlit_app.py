cfb80a7f253e45970b4d9da023b8a48adad4ce58
noamephrat94 <noamephrat94@gmail.com>
2023-02-22 17:32:15 +0200

    diff test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,22 @@
 import streamlit as st
-import os, sys
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-_ = installff()
-from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
 
-browser.get('http://example.com')
-st.write(browser.page_source)
+driver = get_driver()
+driver.get('http://www.google.com')
+
+st.code(driver.page_source)
 
 
 
