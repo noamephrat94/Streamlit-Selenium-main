361c9e9e1c54761d494b2ca0a53ff83de43c3751
noamephrat94 <noamephrat94@gmail.com>
2023-02-23 14:07:52 +0200

    testing diff method

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ import os
 import time
 import datetime
 from bs4 import BeautifulSoup
+import countries
 
 
 @st.cache_resource
@@ -36,42 +37,43 @@ def get_news(driver, search_term):
     # Query to obtain links
     search_term = search_term.replace(" ", "%20")
     df = create_df()
-    query = 'comprehensive guide to web scraping in python'
-    links = []  # Initiate empty list to capture final results
-    # Specify number of pages on google search, each page contains 10 #links
     n_pages = 2
-    for page in range(1, n_pages):
-        # url = "http://www.google.com/search?q=" + search_term +"&start=" + str((page - 1) * 10)
-        url = f'https://www.bing.com/news/search?q={search_term}&qft=sortbydate%3d%221%22&FORM=HDRSC7'
-        st.write(url)
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        # soup = BeautifulSoup(r.text, 'html.parser')
-        # st.code(soup)
-        search = soup.findAll('div', class_="news-card-body card-with-cluster")
-        # st.code(soup)
-        for d in search:
-            col1, col2 = st.columns([1, 3])
-            # st.code(d)
-            with col1:
-                try:
-                    img = d.find('div', 'image right').find('a').find('img')['src']
-                    st.image(img)
-                except Exception as e:
-                    st.write('No Image')
-            with col2:
-                url = d.find('a', class_='title')['href']
-                title = d.find('a', class_='title').text
-                paragraph = d.find('div', class_='snippet').text
-                try:
-                    age = d.find('div', class_='source set_top').findAll("span")[-1].text
-                except Exception as e:
-                    age = None
-                st.subheader(title)
-                st.markdown(paragraph)
-                if age:
-                    st.text(age)
-            st.markdown("""---""")
+    for countrie in countries.countries:
+        for page in range(1, n_pages):
+            # url = "http://www.google.com/search?q=" + search_term +"&start=" + str((page - 1) * 10)
+            url = f'https://www.bing.com/news/search?q={search_term}%20{countrie}&qft=sortbydate%3d%221%22&FORM=HDRSC7'
+            st.write(url)
+            driver.get(url)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            # soup = BeautifulSoup(r.text, 'html.parser')
+            # st.code(soup)
+            search = soup.findAll('div', class_="news-card-body card-with-cluster")
+            search = search[0:5]
+            # st.code(soup)
+            for d in search:
+                col1, col2 = st.columns([1, 3])
+                # st.code(d)
+                with col1:
+                    try:
+                        img = d.find('div', 'image right').find('a').find('img')['src']
+                        st.image(img)
+                    except Exception as e:
+                        st.write('No Image')
+                with col2:
+                    url = d.find('a', class_='title')['href']
+                    title = d.find('a', class_='title').text
+                    paragraph = d.find('div', class_='snippet').text
+                    try:
+                        age = d.find('div', class_='source set_top').findAll("span")[-1].text
+                    except Exception as e:
+                        age = None
+                    st.subheader(title)
+                    st.markdown(paragraph)
+                    if age:
+                        st.text(age)
+                    link = f'[Read Article]({url})'
+                    st.markdown(link, unsafe_allow_html=True)
+                st.markdown("""---""")
     return df
 def create_ds(data, search_term):
     df = pd.DataFrame(data)
