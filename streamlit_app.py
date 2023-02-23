77c58ae38c001819628d0c9a3a34aee3a8faa63d
noamephrat94 <noamephrat94@gmail.com>
2023-02-23 11:30:04 +0200

    diff test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -7,11 +6,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @st.cache_resource
 def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
+# options = Options()
+# options.add_argument('--disable-gpu')
+# options.add_argument('--headless')
 
 driver = get_driver()
 driver.get('http://www.google.com')
