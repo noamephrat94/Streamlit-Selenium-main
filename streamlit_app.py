374c88f2f62165d4b45609acf8d1b9a86db1f1df
noamephrat94 <noamephrat94@gmail.com>
2023-02-23 11:26:36 +0200

    diff test

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 
 driver = get_driver()
-driver.get('http://google.com')
+driver.get('http://www.google.com')
 
 st.code(driver.page_source)
